72affba6a25de7456b8d2db215e395640c858a5f
Anthonyfma <anthony.voilque@gmail.com>
2016-08-12 15:54:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
-
+volatile int state = LOW; // déclartion d'une variable volatile
 
 
 void setup()
 {
 // Défini l'interruption
-  attachInterrupt(0, blink, CHANGE); // attache l'interruption externe n°0 à la fonction blink
+  attachInterrupt(0, interupteur, CHANGE); // attache l'interruption externe n°0 à la fonction blink
+}
+
+void loop()
+{
+  
+  if (state==HIGH)
+  {
+    
+    
+    
+    
+  }
+  
+  delay(100);
 }
 
 
 // Fonction lancée au moment de l'interruption
-void blink() // la fonction appelée par l'interruption externe n°0
+void interupteur() // la fonction appelée par l'interruption externe n°0
 {
   state = !state; // inverse l'état de la variable
 }
