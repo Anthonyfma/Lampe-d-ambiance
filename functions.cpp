159082b127b29c53f2c6a7267df0fb64c9e04e26
Anthonyfma <anthony.voilque@gmail.com>
2016-08-17 18:12:46 +0200

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,29 +1,13 @@
 int min_vague=180;
 
-
-
-void lumiere_blanche__vague_bleue()
+void lumiere_blanche()
 {
-  p=0.2+0.008*(potar-300);
+  p=0.2+0.008*(potar);
   
-  for (int i=min_vague; i<=254; i=i+1)
-  {
-    // Avant
-    digitalWrite(bluePin2, i);
-    // Arrière
-    digitalWrite(redPin1, 254*p);
-    digitalWrite(greenPin1, 254*p);
-    digitalWrite(bluePin1, 254*p);
-  }
-  for (int i=254; i<=min_vague; i=i-1)
-  {
-    // Avant
-    digitalWrite(bluePin2, i);
-    // Arrière
-    digitalWrite(redPin1, 254*p);
-    digitalWrite(greenPin1, 254*p);
-    digitalWrite(bluePin1, 254*p);
-  }
+  digitalWrite(redPin1, 254*p);
+  digitalWrite(greenPin1, 254*p);
+  digitalWrite(bluePin1, 254*p);
+  
 }
 
 
@@ -48,7 +32,68 @@ void vague_bleue()
 }
 
 
+
+void lumiere_blanche__vague_bleue()
+{
+  p=0.2+0.008*(potar-300);
+  
+  for (int i=min_vague; i<=254; i=i+1)
+  {
+    // Avant
+    digitalWrite(bluePin1, i);
+    // Arrière
+    digitalWrite(redPin2, 254*p);
+    digitalWrite(greenPin2, 254*p);
+    digitalWrite(bluePin2, 254*p);
+  }
+  for (int i=254; i<=min_vague; i=i-1)
+  {
+    // Avant
+    digitalWrite(bluePin1, i);
+    // Arrière
+    digitalWrite(redPin2, 254*p);
+    digitalWrite(greenPin2, 254*p);
+    digitalWrite(bluePin2, 254*p);
+  }
+}
+
+
+void vague_bleue()
+{
+  p=0.2+0.008*(potar-300);
+  
+  for (int i=min_vague; i<=254; i=i+1)
+  {
+    // Avant
+    digitalWrite(bluePin1, i);
+    // Arrière
+    digitalWrite(bluePin2, i);
+  }
+  for (int i=254; i<=min_vague; i=i-1)
+  {
+    // Avant
+    digitalWrite(bluePin1, i);
+    // Arrière
+    digitalWrite(bluePin2, i);
+  }
+}
+
 void vague_cyan()
+{
+  p=0.2+0.008*(potar-300);
+  for (int i=min_vague; i<=254; i=i+1)
+  {
+    digitalWrite(bluePin1, i);
+    digitalWrite(greenPin1, i);
+  }
+  for (int i=254; i<=min_vague; i=i-1)
+  {
+    digitalWrite(bluePin1, i);
+    digitalWrite(greenPin1, i);
+  }
+}
+
+void vague_cyan__cyan()
 {
   p=0.2+0.008*(potar-300);
   
